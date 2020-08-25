ffc2d64abda10d7e18ee6b87804747ecda7c3bb4
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2020-08-25 16:32:01 -0400

    Scattered Work

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Notes(models.Model):
+    text = models.TextField()
+    title = models.CharField(max_length=255)
+    when_created = models.DateTimeField(auto_now_add=True)
+    when_edited = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(user, on_delete=models.CASCADE, related_names='notes')
+    categories = models.
