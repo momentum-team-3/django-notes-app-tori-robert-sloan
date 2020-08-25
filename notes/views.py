25cecaab41d09dbedc51e80d359a2133dd749b24
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2020-08-25 16:39:05 -0400

    Actual Work

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
+def notes_list(request):
+    user = request.user
+    notes = user.notes.all()
+    return render(request, ' notes/list_notes.html', {'notes': notes})
+
+def notes_detail()
+
+def add_note(request):
+    form = NoteForm()
+    if request.method == 'POST':
+        form = NoteForm(data=request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
+            return redirect('show_note', pk=note.pk)
+    else:
+        form=NoteForm()
+        return render(request, 'notes/add_note.html',{'form':form})
--- team_project/urls.py
+++ team_project/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', notes_detail.notes_list, name = "notes_list")
 ]
