984ee6638652c7fede371933e633d022d55ea484
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2020-08-26 11:19:44 -0400

    Trying to work

--- notes/views.py
+++ notes/views.py
@@ -8,18 +8,7 @@ def notes_list(request):
     return render(request, ' notes/list_notes.html', {'notes': notes})
 
 def notes_detail(request, pk)
-note = Note.objects.get(pk=pk)
-return render(request, 'notes_detail.html', {'Notes':notes})
+model = Notes
 
 def add_note(request):
-    form = NoteForm()
-    if request.method == 'POST':
-        form = NoteForm(data=request.POST)
-        if form.is_valid():
-            note = form.save(commit=False)
-            note.user = request.user
-            note.save()
-            return redirect('show_note', pk=note.pk)
-    else:
-        form=NoteForm()
-        return render(request, 'notes/add_note.html',{'form':form})
+    model = Notes
